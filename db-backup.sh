b0927ce585fb04ecaf152fd00d91caf0ed821d92
Guru <guru@duck.com>
2024-06-01 23:13:44 +0000

    use github secrets

--- db-backup.sh
+++ db-backup.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-host=""
-port=
-db_user=""
-db_pass=""
-db_name=""
-zip_pass=""
-tg_bot_token=""
-tg_chat_id=""
-
-current_time=$(date "+%Y-%m-%d_%H-%M-%S")
-file_name="${db_name}-${current_time}"
-mariadb-dump --no-tablespaces -h "${host}" -P ${port} -u "${db_user}" -p"${db_pass}" "${db_name}" > "${file_name}.sql"
+mariadb-dump --no-tablespaces -h "${host}" -P ${port} -u "${db_user}" -p"${db_pass}" "${db_name}" > temp.sql
+current_time="$(date "+%Y-%m-%d_%H-%M-%S")"
+file_name="${db_name}_${current_time}"
+mv temp.sql "${file_name}.sql"
 zip -e -P "${zip_pass}" "${file_name}.zip" "${file_name}.sql" > /dev/null
 curl -s -F chat_id="${tg_chat_id}" -F document=@"${file_name}.zip" "https://api.telegram.org/bot${tg_bot_token}/sendDocument" > /dev/null
 rm "${file_name}".*
