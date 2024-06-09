f8d71866bca8d52eb8e2f682b20662c341dbad65
Guru <guru@duck.com>
2024-06-09 01:57:53 +0000

    Update db-backup.sh

--- db-backup.sh
+++ db-backup.sh
@@ -6,9 +6,9 @@ old_file_name="$(date +"%Y-%m-%d" --date="7 days ago")"
 mv temp.sql "${file_name}.sql"
 zip -e -P "${zip_pass}" "${file_name}.zip" "${file_name}.sql" > /dev/null
 echo "CONFIGFILE_VERSION=2.0" > ~/.dropbox_uploader
-echo "OAUTH_APP_KEY=${DROPBOX_APP_KEY}" >> ~/.dropbox_uploader
-echo "OAUTH_APP_SECRET=${DROPBOX_APP_SECRET}" >> ~/.dropbox_uploader
-echo "OAUTH_REFRESH_TOKEN=${DROPBOX_REFRESH_TOKEN}" >> ~/.dropbox_uploader
+echo "OAUTH_APP_KEY=${dropbox_app_key}" >> ~/.dropbox_uploader
+echo "OAUTH_APP_SECRET=${dropbox_app_secret}" >> ~/.dropbox_uploader
+echo "OAUTH_REFRESH_TOKEN=${dropbox_refresh_token}" >> ~/.dropbox_uploader
 curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh > /dev/null
 chmod +x dropbox_uploader.sh
 ./dropbox_uploader.sh upload "${file_name}.zip" "${file_name}.zip"
