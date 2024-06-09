e7134042554b22f7f5f9f1a10ad6b554f96a7b03
Guru <guru@duck.com>
2024-06-09 01:01:23 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,34 +1,16 @@
 #!/bin/bash
 
-# Get the date 7 days ago. Used to delete the redundant backup file.
-old_date=$(date +"%Y-%m-%d" --date="7 days ago")
-
 echo "CONFIGFILE_VERSION=2.0" > ~/.dropbox_uploader
 echo "OAUTH_APP_KEY=${OAUTH_APP_KEY}" >> ~/.dropbox_uploader
 echo "OAUTH_APP_SECRET=${OAUTH_APP_SECRET}" >> ~/.dropbox_uploader
 echo "OAUTH_REFRESH_TOKEN=${OAUTH_REFRESH_TOKEN}" >> ~/.dropbox_uploader
-echo "The configuration has been saved."
-
-git clone https://github.com/andreafabrizi/Dropbox-Uploader
-echo "git cloned"
-
+curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
+chmod +x dropbox_uploader.sh
 mariadb-dump --no-tablespaces -h "${host}" -P ${port} -u "${db_user}" -p"${db_pass}" "${db_name}" > temp.sql
-echo "dumped"
-current_time="$(date "+%Y-%m-%d")"
-file_name="${current_time}"
+file_name="$(date "+%Y-%m-%d")"
+old_file_name="$(date +"%Y-%m-%d" --date="7 days ago")"
 mv temp.sql "${file_name}.sql"
-zip -e -P "${zip_pass}" "${file_name}.zip" "${file_name}.sql" > /dev/null
-echo "zipped"
-
-echo "Uploading backup tarball to Dropbox..."
-./Dropbox-Uploader/dropbox_uploader.sh upload "${file_name}.zip"
-echo "uploaded"
-
-# Delete the old backup
-echo "Deleting old Dropbox backup..."
-./Dropbox-Uploader/dropbox_uploader.sh delete $old_date.zip
-echo "deleted"
-
+zip -e -P "${zip_pass}" "${file_name}.zip" "${file_name}.sql"
+./dropbox_uploader.sh upload "${file_name}.zip" "${file_name}.zip"
+./dropbox_uploader.sh delete "${old_file_name}.zip"
 rm "${file_name}".*
-
-echo "Finished"
