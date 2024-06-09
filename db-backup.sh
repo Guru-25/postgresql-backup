d95bdd98cfae0449d9e97a4f49b7a832c1938c73
Guru <guru@duck.com>
2024-06-09 01:26:15 +0000

    Update db-backup.sh

--- db-backup.sh
+++ db-backup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 mariadb-dump --no-tablespaces -h "${host}" -P ${port} -u "${db_user}" -p"${db_pass}" "${db_name}" > temp.sql
-current_time="$(date "+%Y-%m-%d_%H-%M-%S")"
-file_name="${db_name}_${current_time}"
+file_name="$(date "+%Y-%m-%d")"
+old_file_name="$(date +"%Y-%m-%d" --date="7 days ago")"
 mv temp.sql "${file_name}.sql"
 zip -e -P "${zip_pass}" "${file_name}.zip" "${file_name}.sql" > /dev/null
-curl -s -F chat_id="${tg_chat_id}" -F document=@"${file_name}.zip" "https://api.telegram.org/bot${tg_bot_token}/sendDocument" > /dev/null
+echo "CONFIGFILE_VERSION=2.0" > ~/.dropbox_uploader
+echo "OAUTH_APP_KEY=${DROPBOX_OAUTH_APP_KEY}" >> ~/.dropbox_uploader
+echo "OAUTH_APP_SECRET=${DROPBOX_OAUTH_APP_SECRET}" >> ~/.dropbox_uploader
+echo "OAUTH_REFRESH_TOKEN=${DROPBOX_OAUTH_REFRESH_TOKEN}" >> ~/.dropbox_uploader
+curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh > /dev/null
+chmod +x dropbox_uploader.sh
+./dropbox_uploader.sh upload "${file_name}.zip" "${file_name}.zip"
+./dropbox_uploader.sh delete "${old_file_name}.zip"
 rm "${file_name}".*
