0d42e0aa2f06397d9afd705bd3161abc80fcf750
Guru <guru@duck.com>
2024-06-09 02:16:54 +0000

    Update db-backup.sh

--- db-backup.sh
+++ db-backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mariadb-dump --no-tablespaces -h "${host}" -P ${port} -u "${db_user}" -p"${db_pass}" "${db_name}" > temp.sql
-file_name="$(date "+%Y-%m-%d")"
-old_file_name="$(date +"%Y-%m-%d" --date="7 days ago")"
+current_time="$(date "+%Y-%m-%d_%H-%M-%S")"
+file_name="${db_name}_${current_time}"
 mv temp.sql "${file_name}.sql"
 zip -e -P "${zip_pass}" "${file_name}.zip" "${file_name}.sql" > /dev/null
 echo "CONFIGFILE_VERSION=2.0" > ~/.dropbox_uploader
@@ -11,6 +11,6 @@ echo "OAUTH_APP_SECRET=${dropbox_app_secret}" >> ~/.dropbox_uploader
 echo "OAUTH_REFRESH_TOKEN=${dropbox_refresh_token}" >> ~/.dropbox_uploader
 curl -s "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
 chmod +x dropbox_uploader.sh
-./dropbox_uploader.sh upload "${file_name}.zip" "${file_name}.zip"
-./dropbox_uploader.sh delete "${old_file_name}.zip"
+./dropbox_uploader.sh upload "${file_name}.zip" "$(date "+%Y-%m-%d").zip"
+./dropbox_uploader.sh delete "$(date +"%Y-%m-%d" --date="7 days ago").zip"
 rm "${file_name}".*
