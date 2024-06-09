fd1d3eaa1c7656a743bbda44160368b4bb3f96af
Guru <guru@duck.com>
2024-06-09 02:45:39 +0000

    Update db-backup.sh

--- db-backup.sh
+++ db-backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mariadb-dump --no-tablespaces -h "${host}" -P ${port} -u "${db_user}" -p"${db_pass}" "${db_name}" > temp.sql
+mariadb-dump --no-tablespaces -h "${host}" -P ${port} -u "${user}" -p"${password}" "${database_name}" > temp.sql
 current_time="$(date "+%Y-%m-%d_%H-%M-%S")"
-file_name="${db_name}_${current_time}"
+file_name="${database_name}_${current_time}"
 mv temp.sql "${file_name}.sql"
-zip -e -P "${zip_pass}" "${file_name}.zip" "${file_name}.sql" > /dev/null
+zip -e -P "${zip_password}" "${file_name}.zip" "${file_name}.sql" > /dev/null
 echo "CONFIGFILE_VERSION=2.0" > ~/.dropbox_uploader
 echo "OAUTH_APP_KEY=${dropbox_app_key}" >> ~/.dropbox_uploader
 echo "OAUTH_APP_SECRET=${dropbox_app_secret}" >> ~/.dropbox_uploader
