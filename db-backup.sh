5c70c7f1beb54ffdf6ec933c8f79ae8e624bf70a
Guru <guru@duck.com>
2024-06-09 02:01:06 +0000

    Update db-backup.sh

--- db-backup.sh
+++ db-backup.sh
@@ -9,7 +9,7 @@ echo "CONFIGFILE_VERSION=2.0" > ~/.dropbox_uploader
 echo "OAUTH_APP_KEY=${dropbox_app_key}" >> ~/.dropbox_uploader
 echo "OAUTH_APP_SECRET=${dropbox_app_secret}" >> ~/.dropbox_uploader
 echo "OAUTH_REFRESH_TOKEN=${dropbox_refresh_token}" >> ~/.dropbox_uploader
-curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh > /dev/null
+curl -s "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
 chmod +x dropbox_uploader.sh
 ./dropbox_uploader.sh upload "${file_name}.zip" "${file_name}.zip"
 ./dropbox_uploader.sh delete "${old_file_name}.zip"
